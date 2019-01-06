1a4846ee381e6d6a789a706429ebdebbdc0dfe00
MengTianjian <tim40@pitt.edu>
2019-01-06 08:49:04 -0500

    add reward to go policy gradient

---
+++ spinup/examples/pg_math/2_rtg_pg.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import gym
+from gym.spaces import Discrete, Box
+
+class MLP(nn.Module):
+    def __init__(self, obs_dim, sizes, activation=nn.Tanh, output_activation=None):
+        super(MLP, self).__init__()
+        sizes = [obs_dim] + sizes
+        layers = nn.ModuleList()
+        for i in range(len(sizes)-2):
+            layers.append(nn.Linear(sizes[i], sizes[i+1]))
+            if activation is not None:
+                layers.append(activation())
+        layers.append(nn.Linear(sizes[-2], sizes[-1]))
+        if output_activation is not None:
+            layers.append(output_activation())
+        self.mlp = nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.mlp(x)
+        return out
+
+def reward_to_go(rews):
+    n = len(rews)
+    rtgs = np.zeros_like(rews)
+    for i in reversed(range(n)):
+        rtgs[i] = rews[i] + (rtgs[i+1] if i+1 < n else 0)
+    return rtgs
+
+def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, 
+          epochs=50, batch_size=5000, render=False):
+
+    # make environment, check spaces, get obs / act dims
+    env = gym.make(env_name)
+    assert isinstance(env.observation_space, Box), \
+        "This example only works for envs with continuous state spaces."
+    assert isinstance(env.action_space, Discrete), \
+        "This example only works for envs with discrete action spaces."
+
+    obs_dim = env.observation_space.shape[0]
+    n_acts = env.action_space.n
+
+    # make core of policy network
+    policy_network = MLP(obs_dim, sizes=hidden_sizes+[n_acts])
+
+    # make train optimizer
+    optimizer = torch.optim.Adam(policy_network.parameters(), lr=lr)
+
+    # for training policy
+    def train_one_epoch():
+        # make some empty lists for logging.
+        batch_obs = []          # for observations
+        batch_log_probs = []    # for log probabilities
+        batch_acts = []         # for actions
+        batch_weights = []      # for reward-to-go weighting in policy gradient
+        batch_rets = []         # for measuring episode returns
+        batch_lens = []         # for measuring episode lengths
+
+        # reset episode-specific variables
+        obs = env.reset()       # first obs comes from starting distribution
+        done = False            # signal from environment that episode is over
+        ep_rews = []            # list for rewards accrued throughout ep
+
+        # render first episode of each epoch
+        finished_rendering_this_epoch = False
+
+        # collect experience by acting in the environment with current policy
+        while True:
+
+            # rendering
+            if (not finished_rendering_this_epoch) and render:
+                env.render()
+
+            # save obs
+            batch_obs.append(obs.copy())
+
+            # act in the environment
+            logits = policy_network(torch.tensor(obs).view(1,-1).float())
+            prob = F.softmax(logits, dim=-1)
+            act = torch.multinomial(prob, 1)[0]
+            batch_log_probs.append(F.cross_entropy(logits, act))
+            obs, rew, done, _ = env.step(act.item())
+
+            # save action, reward
+            batch_acts.append(act)
+            ep_rews.append(rew)
+
+            if done:
+                # if episode is over, record info about episode
+                ep_ret, ep_len = sum(ep_rews), len(ep_rews)
+                batch_rets.append(ep_ret)
+                batch_lens.append(ep_len)
+
+                # the weight for each logprob(a_t|s_t) is reward-to-go from t
+                batch_weights += list(reward_to_go(ep_rews))
+
+                # reset episode-specific variables
+                obs, done, ep_rews = env.reset(), False, []
+
+                # won't render again this epoch
+                finished_rendering_this_epoch = True
+
+                # end experience loop if we have enough of it
+                if len(batch_obs) > batch_size:
+                    break
+
+        # take a single policy gradient update step
+        optimizer.zero_grad()
+        batch_losses = []
+        for i in range(len(batch_acts)):
+            batch_losses.append(batch_log_probs[i]*batch_weights[i])
+        loss = sum(batch_losses)/len(batch_losses)
+        loss.backward()
+        optimizer.step()
+        return loss.detach(), batch_rets, batch_lens
+
+    # training loop
+    for i in range(epochs):
+        batch_loss, batch_rets, batch_lens = train_one_epoch()
+        print('epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f'%
+                (i, batch_loss, np.mean(batch_rets), np.mean(batch_lens)))
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env_name', '--env', type=str, default='CartPole-v0')
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument('--lr', type=float, default=1e-2)
+    args = parser.parse_args()
+    print('\nUsing reward-to-go formulation of policy gradient.\n')
+    train(env_name=args.env_name, render=args.render, lr=args.lr)
